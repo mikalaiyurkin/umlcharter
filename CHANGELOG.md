d2c5a722d70f23f9cbe87d93e153913762648522
mikalai yurkin <yurkin.mikalai@gmail.com>
2024-04-02 19:27:25 +0300

    new CI steps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
-### [0.0.1] - 2023.04.02
+### [0.0.3] - 2023.04.02
 
-The first version of the package supporting the sequence diagrams rendered to the Mermaid DSL
+- Add the documentation with the examples for the sequence diagram. 
+- Add the tests for the CI and the check for the supported versions 
+
+### [0.0.2] - 2023.04.02
+
+The first published version of the package supporting the sequence diagrams rendered to the Mermaid DSL
--- setup.py
+++ setup.py
@@ -17,8 +17,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mikalaiyurkin/charter",
     packages=setuptools.find_packages(),
+    python_requires=">=3.9",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
